877cd07220c927cd941b5764f319b5695a7b2f10
Walter Han <wappuchino@gmail.com>
2018-02-24 20:12:15 -0600

    Validators

--- LCMiner/LCMiner/src/LCMiner.java
+++ LCMiner/LCMiner/src/LCMiner.java
@@ -1,6 +1,6 @@
 
 public class LCMiner {
-	public static final void main(final String[] args)
+	public static final void main(final String[] args) throws Exception
 	{
 		System.out.println("+===============================================+");
 		System.out.println("|Lendcoin Miner v0.1.1, made with love by DaBois|");
@@ -11,6 +11,6 @@ public class LCMiner {
 		
 		Transaction tc = new Transaction("428acdddde84", "494902409865", 30, 10, 5, 100, "093034", "4309548D55");
 		Block bk = new Block(0, 1, 1, 1, "49059873456879654783989745", "891234", new Transaction[] {tc}, "13", "298455");
-		System.out.println(bk);
+		System.out.println(tc.validateReceiver() && tc.validateSender());
 	}
 }
